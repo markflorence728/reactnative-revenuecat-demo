58d55f9bd1060d33f3c5147392d83743fa25a168
markflorence728 <markflorence728@gmail.com>
2024-07-09 03:25:38 +0800

    feat: setup ios revenuecat

--- ios/reactnativerevenuecatdemo/AppDelegate.h
+++ ios/reactnativerevenuecatdemo/AppDelegate.h
@@ -1,5 +1,11 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <Purchases/Purchases.h>
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [Purchases configureWithAPIKey:@"revenuecat_api_key"];
+  return YES;
+}
 
 @interface AppDelegate : RCTAppDelegate
 
